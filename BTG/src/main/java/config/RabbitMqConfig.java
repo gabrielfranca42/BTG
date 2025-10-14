9395bd82b8677ddc5a9e88696e742312e3ae84f8
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-14 09:34:05 -0300

    consertando spring inicializar com docker compose e criacao de fila com rabbitMQ

--- BTG/src/main/java/config/RabbitMqConfig.java
+++ BTG/src/main/java/config/RabbitMqConfig.java
@@ -1,6 +1,8 @@
 package config;
 
 
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,4 +18,9 @@ public class RabbitMqConfig {
         return new Jackson2JsonMessageConverter();
     }
 
+
+    @Bean
+    public Declarable oderCreatedQueue(){
+        return new Queue(ORDER_CREATED_QUEUE);
+    }
 }
--- BTG/src/main/java/dto/OderItemEventDto.java
+++ BTG/src/main/java/dto/OderItemEventDto.java
@@ -2,6 +2,7 @@ package dto;
 
 import java.util.List;
 
+
 public record OderItemEventDto(Long codigoPedido,
                                Long codigoCliente,
                                List<OderCreatedEventDto> items) {
