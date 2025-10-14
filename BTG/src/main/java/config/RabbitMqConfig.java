d9e08c821b745a11f4ca5198536c846d8e40ec28
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-13 22:35:56 -0300

    criando jackson2jsonmessageconverter

--- BTG/src/main/java/config/RabbitMqConfig.java
+++ BTG/src/main/java/config/RabbitMqConfig.java
@@ -1,11 +1,19 @@
 package config;
 
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 
 @Configuration
 public class RabbitMqConfig {
 
     public static final String ORDER_CREATED_QUEUE = "btg-pactual-oder-created";
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
 }
