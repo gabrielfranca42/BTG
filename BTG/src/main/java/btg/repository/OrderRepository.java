1c92fa0e812c61807032920eb704904a066f33b2
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-20 10:15:03 -0300

    criando repository

--- BTG/src/main/java/btg/repository/OrderRepository.java
+++ BTG/src/main/java/btg/repository/OrderRepository.java
@@ -1,4 +1,11 @@
 package btg.repository;
 
-public interface OrderRepository {
+import btg.model.OrderModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface OrderRepository extends MongoRepository<OrderModel, Long> {
+
+    Page<OrderModel> findAllByCustomerId(Long customerId, PageRequest pageRequest);
 }
--- BTG/src/main/java/btg/service/OrderService.java
+++ BTG/src/main/java/btg/service/OrderService.java
@@ -1,4 +1,10 @@
 package btg.service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+
+
 }
