51842d8e684148ba473a1ef9fba0c05a6afdede6
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-20 09:37:03 -0300

    criando controller

--- BTG/src/main/java/btg/controller/OrderController.java
+++ BTG/src/main/java/btg/controller/OrderController.java
@@ -1,4 +1,42 @@
 package btg.controller;
 
+
+import btg.controller.dto.ApiResponse;
+import btg.controller.dto.OrderResponse;
+import btg.service.OrderService;
+import org.apache.coyote.Response;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+
+    @GetMapping("/customers/{custumerId}/orders")
+    public ResponseEntity<ApiResponse<OrderResponse>> listOders(@PathVariable("customerId") Long customerId,
+                                                                @RequestParam(name = "page", defaultValue = "0") Integer page,
+                                                                @RequestParam(name = "pageSize" , defaultValue = "10") Integer pageSize{
+
+        var pageResponse = orderService.findAllByCustomerId(custpmerId, PageRequest.of(page, pageSize));
+        var totalOnders = orderService.findTotalOnOrdersByCustomerId(customerId);
+
+
+        return ResponseEntity.ok(new ApiResponse<>(
+                Map.of("totalOnOrders", totalOnOrders),
+                pageResponse.getContent(),
+                PaginationResponse.fromPage(pageResponse)
+        ));
+    }
 }
